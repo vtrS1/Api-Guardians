72b91ec2a6b9e8db7fb53c12e2d9b58bcc478440
vtr <vtrvieira2.0@gmail.com>
2023-02-22 22:32:38 -0400

    Feat: Add Cors

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
-FROM node:lts-alpine
-ENV NODE_ENV=production
-WORKDIR /usr/app
-COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
-RUN yarn install 
+FROM node:18.12.1
+WORKDIR /usr/src/app 
+COPY package*.json ./
+RUN yarn install
 COPY . .
-EXPOSE 3000
-RUN yarn install 
-USER node
-CMD ["yarn", "start:dev"]
+EXPOSE 8080
+CMD [ "yarn", "start:dev" ]
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ dotenv.config();
 import db from "./src/models";
 import express from "express";
 import routes from "./src/routes";
+import cors from "cors";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, async () => {
+app.listen(3000, async () => {
   try {
     await db.sequelize.authenticate();
     console.log("Connection has been established successfully.");
